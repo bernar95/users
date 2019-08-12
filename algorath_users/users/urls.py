3dad7bf27cfe51ed329c4cf32a78e1139360f1a0
Bernardo <berny.sevillista@gmail.com>
2019-08-12 18:42:34 +0200

    Changes in the index view. New creation form. New connections view.

--- algorath_users/users/urls.py
+++ algorath_users/users/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:user_id>/connections/', views.connections, name='connections'),
+    path('add_user/', views.add_user, name='add_user'),
 ]
--- algorath_users/users/views.py
+++ algorath_users/users/views.py
@@ -1,12 +1,37 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import User
 
 
 def index(request):
-    latest_user_list = User.objects.order_by('user_id')
-    welcome = '<h2> Welcome to our Users app! </h2> ' \
-              '<h4>These are some of the latest users added to the app: </h4>'
-    output = ', <br>'.join([u.user_name for u in latest_user_list])
-    return HttpResponse(welcome + output)
+    user_list = User.objects.order_by('user_id')
+    context = {'user_list': user_list}
+    return render(request, 'index.html', context)
+
+
+def connections(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'connections.html', {'user': user})
+
+
+def add_user(request):
+    user_list = User.objects.order_by('user_id')
+    try:
+        user_name = request.POST['name']
+        existing_user = User.objects.all().filter(user_name=user_name)
+        if not user_name:
+            raise ValueError
+        if existing_user:
+            raise KeyError
+
+    except ValueError:
+        context = {'error_message': "You didn´t write the name.", 'user_list': user_list}
+        return render(request, 'index.html', context)
+    except KeyError:
+        context = {'error_message': "There is an existing user with that name.", 'user_list': user_list}
+        return render(request, 'index.html', context)
+    else:
+        user = User(user_name=user_name)
+        user.save()
+        return render(request, 'index.html', {'user_list': user_list})
 
