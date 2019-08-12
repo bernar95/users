a17c6df717ff58e51e7e07c508117cbcd2505156
Bernardo <berny.sevillista@gmail.com>
2019-08-12 18:23:58 +0200

    Changes in the user model

--- algorath_users/users/models.py
+++ algorath_users/users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class User(models.Model):
-    user_id = models.IntegerField(primary_key=True)
+    user_id = models.AutoField(primary_key=True)
     user_name = models.TextField(max_length=200)
     user_connections = models.ManyToManyField('self', through='Connection', symmetrical=False,
                                               related_name='related_to')
