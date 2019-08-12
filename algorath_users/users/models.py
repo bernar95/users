3e2e8a57b1b76b2a469248ad3ed133bf59906d4c
Bernardo <berny.sevillista@gmail.com>
2019-08-12 16:44:58 +0200

    Database populated. Index view with welcome message and a list of users

--- algorath_users/users/models.py
+++ algorath_users/users/models.py
@@ -5,6 +5,17 @@ from django.db import models
 
 
 class User(models.Model):
-    user_id = models.IntegerField()
+    user_id = models.IntegerField(primary_key=True)
     user_name = models.TextField(max_length=200)
-    user_connection = models.ForeignKey('self', on_delete=models.DO_NOTHING)
+    user_connections = models.ManyToManyField('self', through='Connection', symmetrical=False,
+                                              related_name='related_to')
+
+    def __str__(self):
+        return "ID: " + str(self.user_id) + ", Name: " + self.user_name
+
+
+class Connection(models.Model):
+    from_user = models.ForeignKey(User, related_name='from_users', on_delete=models.DO_NOTHING)
+    to_user = models.ForeignKey(User, related_name='to_users', on_delete=models.DO_NOTHING)
+
+
--- algorath_users/users/views.py
+++ algorath_users/users/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 
+from .models import User
+
+
 def index(request):
-    return HttpResponse("Welcome! You are at our Users app.")
+    latest_user_list = User.objects.order_by('user_id')
+    welcome = '<h2> Welcome to our Users app! </h2> ' \
+              '<h4>These are some of the latest users added to the app: </h4>'
+    output = ', <br>'.join([u.user_name for u in latest_user_list])
+    return HttpResponse(welcome + output)
 
