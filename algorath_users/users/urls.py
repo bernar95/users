fb5fa346452d14bfcf79865a284bfd72db115e03
Bernardo <berny.sevillista@gmail.com>
2019-08-14 11:18:04 +0200

    New functionalities: find users and connect users.

--- algorath_users/users/urls.py
+++ algorath_users/users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:user_id>/connections/', views.connections, name='connections'),
     path('add_user/', views.add_user, name='add_user'),
+    path('<int:user_id>/connections/find_user/', views.find_user, name='find_user'),
+    path('<int:user_id>/connections/connect_user/', views.connect_user, name='connect_user'),
 ]
--- algorath_users/users/views.py
+++ algorath_users/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import User
+from .models import User, Connection
 
 
 def index(request):
@@ -34,4 +34,42 @@ def add_user(request):
         user = User(user_name=user_name)
         user.save()
         return render(request, 'index.html', {'user_list': user_list})
+
+
+def find_user(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    try:
+        user_name = request.POST['name']
+        if not user_name:
+            raise ValueError
+    except ValueError:
+        context = {'error_message': "You didn´t write the name.", 'user': user}
+        return render(request, 'connections.html', context)
+    else:
+        user_list = User.objects.all().filter(user_name__icontains=user_name)
+        context = {'user': user, 'user_list': user_list}
+        return render(request, 'connections.html', context)
+
+
+def connect_user(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+    try:
+        user_name = request.POST.get('connect_name', False)
+        existing_user = User.objects.all().filter(user_name=user_name)[0]
+        if not user_name:
+            raise ValueError
+        if Connection.objects.all().filter(from_user=user, to_user=existing_user):
+            raise KeyError
+    except ValueError:
+        context = {'error_message': "You didn´t write the name.", 'user': user}
+        return render(request, 'connections.html', context)
+    except KeyError:
+        context = {'error_message': user.user_name + " already has a connection with " + existing_user.user_name + ".",
+                   'user': user}
+        return render(request, 'connections.html', context)
+    else:
+        user.user_connections.add(existing_user)
+        context = {'user': user}
+        return render(request, 'connections.html', context)
+
 
